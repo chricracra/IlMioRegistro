07e571ca985bab57db2ca462c9d9136f872635cd
Christian Conti <145473867+chricracra@users.noreply.github.com>
2025-12-03 12:44:47 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,23 @@
+const CACHE_NAME = 'neoregister-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './images/icon-192.png',
+  './images/icon-512.png'
+  // Nota: Le librerie esterne (Firebase, FontAwesome) vengono gestite dal browser
+];
+
+// Installazione
 self.addEventListener('install', (e) => {
-  console.log('App installata!');
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
 
+// Fetching (Strategia: Network First, poi Cache se offline)
 self.addEventListener('fetch', (e) => {
-  // Questo codice è necessario per far funzionare la PWA
-  e.respondWith(fetch(e.request));
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
 });
